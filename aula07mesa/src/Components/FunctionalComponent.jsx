4a79fc5abf4482a20374056f3cf376775c957679
Wirley Almeida <wirley.s.almeida@gmail.com>
2022-03-10 17:13:55 -0300

    Aula 07 Mesa

---
+++ aula07mesa/src/Components/FunctionalComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+const FunctionalComponent = (props) => {
+
+    const {nome, tarefa} = props;
+
+   return (
+       <li>{nome} trará {tarefa}</li>
+   )
+}
+ 
+export default FunctionalComponent;
